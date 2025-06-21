6116271952d77532930a04d70bb4497f6205869e
Michael <michael.frasunkiewicz@stud.tu-darmstadt.de>
2025-06-22 00:19:55 +0200

    added antenna matrix

--- app/vital_radar/vital_radar/gui/main_window.py
+++ app/vital_radar/vital_radar/gui/main_window.py
@@ -2,20 +2,23 @@ from collections import deque
 
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QComboBox
 from PyQt6.QtCore import QTimer, Qt
-from PyQt6.QtGui import QFontMetrics, QFont
+from PyQt6.QtGui import QFont
 
 from vital_radar.gui.widgets.image_display import ImageDisplayWidget
+from vital_radar.gui.widgets.antenna_matrix import AntennaMatrix, tx_to_rx
 from vital_radar.walabot.connection import initRadar, stopRadar, reconnectRadar
 from vital_radar.walabot.calibration import CalibrationWorker
 import vital_radar.walabot.signal_aquisition as sa
 from vital_radar.processing.display_modes import DisplayMode, computePlotData
-from vital_radar.processing.raw_signal_processing import processRawSignal
-from vital_radar.processing.utils import getStack
+from vital_radar.processing.raw_signal_processing import processRawSignal, downsample_raw
+from vital_radar.processing.utils import getStack, dummy_signal_generator
 
 
 class MainWindow(QMainWindow):
-    """Defines the main window for the radar GUI."""
+    """
+    Defines the main window for the radar GUI.
     
+    """
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Vital Radar")
@@ -32,32 +35,29 @@ class MainWindow(QMainWindow):
         self.image_widget = self._build_plot_area()
         self.main_layout.addWidget(self.image_widget)
 
-        # radar control area
+        # status label
         self.status_label = QLabel("Radar Status: Disconnected")
         
-        mono = QFont("Courier New")
-        fm = QFontMetrics(mono)
-        w = fm.horizontalAdvance("00.0")
-        
+        # frequency label
         self.freq_value = QLabel("00.0")
-        self.freq_value.setFont(mono)
-        self.freq_value.setFixedWidth(w)
-       
-        self.distance_label = QLabel("Distance:")
+        self.freq_value.setFont(QFont("Courier New"))
         
-        self.distance_value = QLabel("00.0")
-        self.distance_value.setFont(mono)
-        self.distance_value.setFixedWidth(w)
-        
-        self.distance_label.setVisible(False)
-        self.distance_value.setVisible(False)
+        # radar control area
         self.main_layout.addWidget(self._build_control_area())
 
+        # antenna matrix area
+        self.matrix = AntennaMatrix(tx_to_rx)
+        self.main_layout.addWidget(self.matrix, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.matrix.selectionChanged.connect(self.on_matrix_change)
+        self.selected_pairs: set[tuple[int,int]] = set()
+        
         # radar and buffer
         self.radar_connected = False
         self.slow_time_N = 50
         self.signal_buffer = deque(maxlen=self.slow_time_N)
-
+        
+        self.dummy_signal_generator = dummy_signal_generator()
+        
         # Initialize radar
         try:
             initRadar()
@@ -70,89 +70,56 @@ class MainWindow(QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.refreshImage)
         self.timer.start(100)
-
-    def _build_plot_area(self):
-        """Returns the widget containing the radar image."""
-        image_w = ImageDisplayWidget()
-        return image_w
-    
-    def _build_control_area(self):
-        """Returns a QWidget with all buttons and labels laid out."""
-        container = QWidget()
-        hbox = QHBoxLayout(container)
-        hbox.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-
-        # Buttons
-        hbox.addWidget(self._build_button("Calibrate", self.calibrateRadar))
-        hbox.addWidget(self._build_button("Reconnect", self.reconnectRadar))
-
-        hbox.addStretch()
-        
-        # Dropdown (centered)
-        self.mode_combo = QComboBox()
-        for mode in DisplayMode:
-            self.mode_combo.addItem(mode.name, mode)
-        self.mode_combo.setCurrentText(self.current_display_mode.name)
-        self.mode_combo.currentIndexChanged.connect(self.modeChanged)
-        hbox.addWidget(self.mode_combo, alignment=Qt.AlignmentFlag.AlignCenter)
-
-        hbox.addWidget(self.distance_label, alignment=Qt.AlignmentFlag.AlignCenter)
-        hbox.addWidget(self.distance_value, alignment=Qt.AlignmentFlag.AlignCenter)
-        
-        hbox.addStretch()
-
-        # Status labels
-        vbox = QVBoxLayout()
-        vbox.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
-        vbox.addWidget(self.status_label)
-        
-        freq_container = QWidget()
-        freq_layout = QHBoxLayout(freq_container)
-        freq_layout.addWidget(QLabel("Trigger Frequency:"))
-        freq_layout.addWidget(self.freq_value)
-        vbox.addWidget(freq_container)
-        hbox.addLayout(vbox)
-
-        return container
-    
-    def _build_button(self, text, slot):
-        btn = QPushButton(text)
-        btn.clicked.connect(slot)
-        return btn
     
     def refreshImage(self):
+        """
+        This function is called repeatedtly as long as the GUI is running and updates the displayed image.
+        
+        """
         # if no radar is connected calling API functions will return an error, so skip
         if not self.radar_connected:
-            return
+            signals = next(self.dummy_signal_generator)
+            
+            # update trigger frequency GUI label
+            self.freq_value.setText(f"--.-")
+        else:
+            if not self.selected_pairs:
+                self.signal_buffer.popleft()
+                return
+            else:
+                tx_list = sorted({tx for tx, _ in self.selected_pairs})
+                rx_list = sorted({rx for _, rx in self.selected_pairs})
+            
+            # get the signals from the walabot API
+            signals = sa.getSignals(tx_list, rx_list)
+            
+            # update trigger frequency GUI label
+            self.freq_value.setText(f"{sa.trigger_freq:04.1f}")
+        
+        # for RAW dislplay mode the signals arent processed
+        if self.current_display_mode == DisplayMode.RAW:
+            signals = downsample_raw(signals, 10)
+        else:
+            # for all other modes the IQ signals are used
+            signals = processRawSignal(signals)
 
-        # choose transmit and receive antennas
-        desired_tx = [1, 17]
-        desired_rx = [2, 6, 10, 14]
-
-        # get the corresponding signals
-        signals = sa.getSignals(desired_tx, desired_rx)
+        # append signals to signal buffer
+        self.signal_buffer.append(signals)
         
-        # process the raw data to downsampled I/Q signals
-        iq_signals = processRawSignal(signals)
-        
-        # append signal to signal matrix (deque also removes oldest signal if full)
-        self.signal_buffer.append(iq_signals)
-        
+        # convert buffer to matrix
         signal_matrix = getStack(self.signal_buffer)
         
-        data, distance = computePlotData(signal_matrix, self.current_display_mode)
-        
-        if distance is not None:
-            self.distance_value.setText(f"{distance:04.1f}")
-        else:
-            self.distance_value.setText("00.0")
-            
-        self.freq_value.setText(f"{sa.trigger_freq:04.1f}")
+        # compute plot data
+        plot_data = computePlotData(signal_matrix, self.current_display_mode)
         
         # update plot
-        self.image_widget.updateImage(data)
-
+        self.image_widget.updateImage(plot_data, self.current_display_mode)
+        
     def calibrateRadar(self):
+        """
+        Slot connected to calibration button.
+        
+        """
         # if no radar is connected calling API functions will return an error, so skip
         if not self.radar_connected:
             return
@@ -162,21 +129,16 @@ class MainWindow(QMainWindow):
             self.calibration_thread.start()
 
     def reconnectRadar(self):
+        """
+        Slot connected to reconnect button.
+        
+        """
         try:
             success = reconnectRadar()
             self.updateStatus(success)
         except Exception as e:
             print("Reconnect failed:", e)
             self.updateStatus(False)
-
-    def modeChanged(self, index):
-        self.current_display_mode = self.mode_combo.currentData()
-        if self.current_display_mode == DisplayMode.DISTANCE:
-            self.distance_label.setVisible(True)
-            self.distance_value.setVisible(True)
-        else:
-            self.distance_label.setVisible(False)
-            self.distance_value.setVisible(False)
         
     def updateStatus(self, connected: bool):
         self.radar_connected = connected
@@ -190,7 +152,89 @@ class MainWindow(QMainWindow):
         self.status_label.style().polish(self.status_label)
 
     def closeEvent(self, event):
+        """
+        Stop event loop and radar when window is closed.
+        
+        """
         self.timer.stop()
         stopRadar()
         event.accept()
+    
+    def modeChanged(self):
+        """
+        Slot connected to mode selection dropdown menu.
         
+        """
+        self.current_display_mode = self.mode_combo.currentData()
+        self.signal_buffer.clear()
+        
+    def on_matrix_change(self, tx: int, rx: int, checked: bool):
+        """
+        Connected to checkbox check.
+        
+        """
+        if checked:
+            self.selected_pairs.add((tx, rx))
+        else:
+            self.selected_pairs.discard((tx, rx))
+            
+        self.signal_buffer.clear()
+    
+    def _build_plot_area(self):
+        """
+        Returns the widget containing the radar image.
+        
+        """
+        image_w = ImageDisplayWidget()
+        return image_w
+    
+    def _build_control_area(self):
+        """
+        Returns a QWidget with all buttons and labels laid out.
+        
+        """
+        container = QWidget()
+        hbox = QHBoxLayout(container)
+        hbox.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+
+        # buttons (left)
+        hbox.addWidget(self._build_button("Calibrate", self.calibrateRadar))
+        hbox.addWidget(self._build_button("Reconnect", self.reconnectRadar))
+
+        # add gap 
+        hbox.addStretch()
+        
+        # dropdown (center)
+        self.mode_combo = QComboBox()
+        for mode in DisplayMode:
+            self.mode_combo.addItem(mode.name, mode)
+        self.mode_combo.setCurrentText(self.current_display_mode.name)
+        self.mode_combo.currentIndexChanged.connect(self.modeChanged)
+        hbox.addWidget(self.mode_combo, alignment=Qt.AlignmentFlag.AlignCenter)
+        
+        # add gap 
+        hbox.addStretch()
+
+        # labels
+        vbox = QVBoxLayout()
+        vbox.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
+        vbox.addWidget(self.status_label, alignment=Qt.AlignmentFlag.AlignRight)
+        
+        freq_layout = QHBoxLayout()
+        freq_layout.addWidget(QLabel("Trigger Frequency: "))
+        freq_layout.addWidget(self.freq_value)
+        freq_layout.addWidget(QLabel("Hz"))
+        vbox.addLayout(freq_layout)
+        hbox.addLayout(vbox)
+
+        return container
+    
+    def _build_button(self, text, slot):
+        """
+        Returns a button widget and connects it to a slot.
+        
+        """
+        btn = QPushButton(text)
+        btn.clicked.connect(slot)
+        return btn
+    
